cf411589245e7a5e6b3f7f465771efa6c95260c1
Dino <164735145+2jiyong@users.noreply.github.com>
2025-09-30 23:36:09 +0900

    Title: 최대 감소 부분 수열, Time: 55ms, Memory: 17MB, Status: Passed - Codetree

--- 250930/최대 감소 부분 수열/longest-decreasing-subsequence.py
+++ 250930/최대 감소 부분 수열/longest-decreasing-subsequence.py
@@ -6,10 +6,10 @@ import sys
 dp = [0]*n
 # dp i 채우기
 for i in range(n):
-    val = 0
+    val = 1
     # dp i 보다 작은애들 비교
     for j in range(i):
         if (m[j]>m[i]):
-            val = max(val,dp[j])
-    dp[i] = val + 1
+            val = max(val,dp[j]+1)
+    dp[i] = val
 print(max(dp))
