d50e559221c4ca09f4187789a873535a40b8f224
Dino <164735145+2jiyong@users.noreply.github.com>
2025-10-05 20:13:05 +0900

    Title: 동전 거슬러주기, Time: 43ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 251005/동전 거슬러주기/coin-change.py
@@ -0,0 +1,17 @@
+N, M = map(int, input().split())
+coins = list(map(int, input().split()))
+
+# Please write your code here.
+import sys
+MAXSIZE = sys.maxsize
+
+dp = [MAXSIZE]*(M+1)
+dp[0] = 0
+for i in range(1, M+1):
+    for coin in coins:
+        if i < coin:
+            continue
+        dp[i] = min(dp[i-coin]+1, dp[i])
+
+print(dp[-1])
+
