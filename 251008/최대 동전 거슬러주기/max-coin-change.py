cd10210d166bc9ba1be171e4a920b966f5bca92d
Dino <164735145+2jiyong@users.noreply.github.com>
2025-10-08 23:48:02 +0900

    Title: 최대 동전 거슬러주기, Time: 55ms, Memory: 17MB, Status: Passed - Codetree

--- 251008/최대 동전 거슬러주기/max-coin-change.py
+++ 251008/최대 동전 거슬러주기/max-coin-change.py
@@ -10,9 +10,12 @@ for i in range(1,M + 1):
     for coin in coins:
         if coin > i :
             continue
-        
+        if dp[i-coin] == 0 and i-coin != 0:
+            continue
         dp[i] = max(dp[i], dp[i-coin] + 1)
 
 if dp[M] == 0:
     dp[M] = -1
+
 print(dp[M])
+
