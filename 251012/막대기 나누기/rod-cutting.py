122e295a584965093df1477a34a6309c11a6d185
Dino <164735145+2jiyong@users.noreply.github.com>
2025-10-12 18:13:15 +0900

    Title: 막대기 나누기, Time: 55ms, Memory: 17MB, Status: Passed - Codetree

--- 251012/막대기 나누기/rod-cutting.py
+++ 251012/막대기 나누기/rod-cutting.py
@@ -7,13 +7,11 @@ dp = [[0] * (n+1) for _ in range(n+1)]
 for i in range(1,n+1):
     for j in range(1, n+1):
         if j<i:
+            dp[i][j] = dp[i-1][j]
             continue
         
-        val = 0 
-        for k in range(1, i):
-            val = max(val, dp[k][j-i])
 
-        dp[i][j] = max(dp[i][j],val + profit[i-1], dp[i][j-i] + profit[i-1])
+        dp[i][j] = max(dp[i][j],dp[i][j-i] + profit[i-1])
 
 ans = 0
 
