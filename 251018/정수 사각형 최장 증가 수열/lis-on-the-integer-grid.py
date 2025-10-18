0ed3436f002cbc59c73fccd2dcd7948612753607
Dino <164735145+2jiyong@users.noreply.github.com>
2025-10-18 19:00:47 +0900

    Title: 정수 사각형 최장 증가 수열, Time: 182ms, Memory: 27MB, Status: Runtime Error - Codetree

--- 251018/정수 사각형 최장 증가 수열/lis-on-the-integer-grid.py
+++ 251018/정수 사각형 최장 증가 수열/lis-on-the-integer-grid.py
@@ -1,41 +1,49 @@
-import sys
-sys.setrecursionlimit(10**6)  
-
+# Variable declaration and input:
 n = int(input())
-grid = [list(map(int, input().split())) for _ in range(n)]
-
-# Please write your code here.
-# dpij를 해당 셀에서 갈 수 있는 최대값
-
-dp = [[-1]*n for _ in range(n)]
-
-dxs, dys = [1,-1,0,0], [0,0,1,-1]
-
-def in_range(r,c):
-    return r>=0 and r<n and c>=0 and c<n
+grid = [
+    list(map(int, input().split()))
+    for _ in range(n)
+]
+dp = [
+    [-1] * n
+    for _ in range(n)
+]
 
 
-def calc_count(r,c):
-    if dp[r][c] != -1:
-        return dp[r][c]
-    
-    num = 0 
+def in_range(x, y):
+    return 0 <= x and x < n and 0 <= y and y < n
 
-    for dx,dy in zip(dxs,dys):
-        nr = r+dx
-        nc = c+dy
-        if in_range(nr,nc) and grid[r][c] < grid[nr][nc]:
-            num = max(num, calc_count(nr,nc))
-    dp[r][c] = num + 1
-    return num + 1
-    
 
+# Starting from (x, y), find the maximum number of cells
+# that can be reached while satisfying the conditions.
+def find_max(x, y):
+    # If it has already been calculated,
+    # return that value immediately.
+    if dp[x][y] != -1:
+        return dp[x][y]
+
+    # The default value is itself.
+    best = 1
+
+    # Look at cells in order of increasing integer value
+    # and calculate the optimal number of cells for 4 directions.
+    dxs = [-1, 1,  0, 0]
+    dys = [ 0, 0, -1, 1]
+
+    for dx, dy in zip(dxs, dys):
+        nx, ny = x + dx, y + dy
+        if in_range(nx, ny) and grid[nx][ny] > grid[x][y]:
+            best = max(best, find_max(nx, ny) + 1)
+
+    dp[x][y] = best
+    return dp[x][y]
+
+
+# Update the maximum number of cells that can be moved
+# starting from each cell.
+ans = 0
 for i in range(n):
     for j in range(n):
-        calc_count(i,j)
-
-ans=0
-for d in dp:
-    ans = max(ans,max(d))
+        ans = max(ans, find_max(i, j))
 
 print(ans)
