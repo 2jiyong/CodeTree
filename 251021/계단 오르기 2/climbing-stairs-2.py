b1f6c270003bc6ab214dbca44260e248297a105b
Dino <164735145+2jiyong@users.noreply.github.com>
2025-10-21 14:30:42 +0900

    Title: 계단 오르기 2, Time: 56ms, Memory: 17MB, Status: Passed - Codetree

--- 251021/계단 오르기 2/climbing-stairs-2.py
+++ 251021/계단 오르기 2/climbing-stairs-2.py
@@ -2,21 +2,23 @@ n = int(input())
 coin = [0] + list(map(int, input().split()))
 
 # Please write your code here.
-dp =[[0]*(4) for _ in range(n+1)]
+import sys
+MIN = -sys.maxsize
+dp =[[MIN]*(4) for _ in range(n+1)]
 
-dp[1][0] = 0
+dp[0][0] = 0
 dp[1][1] = coin[1]
-dp[1][2] = coin[1]
-dp[1][3] = 0
 
 for i in range(2, n+1):
     for j in range(4):
-        if j == 0:
+        if j == 0 and dp[i-2][j] != MIN:
             dp[i][j] = max(dp[i][j], dp[i-2][j] + coin[i])
             continue
-        
-        dp[i][j] =  max(dp[i][j], dp[i-2][j] + coin[i], dp[i-1][j-1] + coin[i])
-
+        if dp[i-2][j] != MIN:
+            dp[i][j] =  max(dp[i][j], dp[i-2][j] + coin[i])
+        if dp[i-1][j-1] != MIN:
+            dp[i][j] =  max(dp[i][j], dp[i-1][j-1] + coin[i])
+            
 # for d in dp:
 #     print(d)
 
