f393049230375f28454a785cad705de5b2671f87
Dino <164735145+2jiyong@users.noreply.github.com>
2025-10-21 13:57:53 +0900

    Title: 계단 오르기 2, Time: 55ms, Memory: 17MB, Status: Wrong Answer - Codetree

--- 251021/계단 오르기 2/climbing-stairs-2.py
+++ 251021/계단 오르기 2/climbing-stairs-2.py
@@ -6,13 +6,17 @@ dp =[[0]*(4) for _ in range(n+1)]
 
 for i in range(1, n+1):
     for j in range(4):
-        if j == 3:
-            dp[i][j] = max(dp[i][j], dp[i-2][j] + coin[i])
+        if i == 1:
+            dp[i][j] = max(dp[i][j], dp[i-1][j-1] + coin[i])
             continue
-        if j == 0 :
+        
+        if j == 0 or j == 3:
             dp[i][j] = max(dp[i][j], dp[i-2][j] + coin[i])
             continue
         
         dp[i][j] =  max(dp[i][j], dp[i-2][j] + coin[i], dp[i-1][j-1] + coin[i])
+
+# for d in dp:
+#     print(d)
 
 print(max(dp[n]))
