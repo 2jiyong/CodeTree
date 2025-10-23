7ec64e0eafd951f0d56457344145fbcfe23699d1
Dino <164735145+2jiyong@users.noreply.github.com>
2025-10-23 13:35:26 +0900

    Title: 고대 보물 지도의 비밀, Time: 50ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 251023/고대 보물 지도의 비밀/secret-of-ancient-treasure-map.py
+++ 251023/고대 보물 지도의 비밀/secret-of-ancient-treasure-map.py
@@ -12,7 +12,6 @@ for i in range(1, n+1):
     for j in range(k+1):
         if numbers[i]>=0 and j==0:
             dp[i][j] = max(dp[i][j], numbers[i]) 
-            continue
         if numbers[i]>=0:
             if dp[i-1][j] == MIN:
                 continue
