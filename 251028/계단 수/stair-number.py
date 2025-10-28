e2125f574e56919d26e31d4905102819a86306ea
Dino <164735145+2jiyong@users.noreply.github.com>
2025-10-28 13:56:03 +0900

    Title: 계단 수, Time: 55ms, Memory: 17MB, Status: Wrong Answer - Codetree

---
+++ 251028/계단 수/stair-number.py
@@ -0,0 +1,24 @@
+n = int(input())
+
+# Please write your code here.
+# dp[i][j] = i번째일 때, 마지막 숫자가 j일 때의 가지수
+
+dp = [[0]*10 for _ in range(n+1)]
+
+
+for i in range(1,10):
+    dp[1][i] = 1
+
+for i in range(2,n+1):
+    for j in range(10):
+        if j != 0:
+            dp[i][j] += dp[i-1][j-1]
+        if j != 9:
+            dp[i][j] += dp[i-1][j+1]
+
+# for d in dp:
+#     print(d)
+
+print(sum(dp[n]))
+
+
