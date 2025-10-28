6d087e601151b867de36cd30f651545f7d4e83fa
Dino <164735145+2jiyong@users.noreply.github.com>
2025-10-28 13:56:51 +0900

    Title: 계단 수, Time: 52ms, Memory: 17MB, Status: Passed - Codetree

--- 251028/계단 수/stair-number.py
+++ 251028/계단 수/stair-number.py
@@ -5,20 +5,20 @@ n = int(input())
 
 dp = [[0]*10 for _ in range(n+1)]
 
-
+mod_num = 10**9 + 7
 for i in range(1,10):
     dp[1][i] = 1
 
 for i in range(2,n+1):
     for j in range(10):
         if j != 0:
-            dp[i][j] += dp[i-1][j-1]
+            dp[i][j] += dp[i-1][j-1] %mod_num
         if j != 9:
-            dp[i][j] += dp[i-1][j+1]
+            dp[i][j] += dp[i-1][j+1]% mod_num
 
 # for d in dp:
 #     print(d)
 
-print(sum(dp[n]))
+print(sum(dp[n])%mod_num)
 
 
