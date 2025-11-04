830fbad2cca004d04ff427a2edcc165a07163ac4
Dino <164735145+2jiyong@users.noreply.github.com>
2025-11-04 14:08:45 +0900

    Title: 축구와 야구, Time: 107ms, Memory: 20MB, Status: Wrong Answer - Codetree

--- 251104/축구와 야구/soccer-and-baseball.py
+++ 251104/축구와 야구/soccer-and-baseball.py
@@ -8,8 +8,9 @@ for _ in range(n):
 
 # Please write your code here.
 # dp[i][j][k] = i 번째 학생 고려했을 떄, 축구는 j명 야구는 k 명 뽑았을 때의 최대 점수
-
-dp = [[[0]*10 for _ in range(12)] for _ in range(n)]
+import sys
+MIN = -sys.maxsize
+dp = [[[MIN]*10 for _ in range(12)] for _ in range(n)]
 
 dp[0][1][0] = s[0]
 dp[0][0][1] = b[0]
@@ -17,7 +18,10 @@ dp[0][0][1] = b[0]
 for i in range(1, n):
     for j in range(12):
         for k in range(10):
-            dp[i][j][k] = max(dp[i-1][j-1][k] + s[i], dp[i-1][j][k-1] + b[i])
+            if dp[i-1][j-1][k] != MIN:
+                dp[i][j][k] = max(dp[i][j][k], dp[i-1][j-1][k] + s[i])
+            if dp[i-1][j][k-1] != MIN:
+                dp[i][j][k] = max(dp[i][j][k], dp[i-1][j][k-1] + b[i])
 
 # for d in dp[n-1]:
 #     print(d)
