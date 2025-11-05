dcb08244b376356786ae7ffd84f320b8441324d3
Dino <164735145+2jiyong@users.noreply.github.com>
2025-11-05 23:28:53 +0900

    Title: 가장 많은 데이터, Time: 173ms, Memory: 31MB, Status: Passed - Codetree

---
+++ 251105/가장 많은 데이터/most-frequent-data.py
@@ -0,0 +1,13 @@
+n = int(input())
+words = [input() for _ in range(n)]
+d = dict()
+ans = 0 
+for word in words:
+    if word not in d:
+        d[word] = 1
+        ans = max(ans,1)
+    else:
+        d[word] += 1
+        ans = max(ans,d[word])
+print(ans)
+# Please write your code here.
