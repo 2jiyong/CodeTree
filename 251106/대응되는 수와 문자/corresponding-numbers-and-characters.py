37bad1533f1e8d2a5181889fdf0ef6958fa84bf8
Dino <164735145+2jiyong@users.noreply.github.com>
2025-11-06 21:31:49 +0900

    Title: 대응되는 수와 문자, Time: 234ms, Memory: 37MB, Status: Passed - Codetree

---
+++ 251106/대응되는 수와 문자/corresponding-numbers-and-characters.py
@@ -0,0 +1,16 @@
+n, m = map(int, input().split())
+
+# Note: Using 1-based indexing for words as per C++ code
+words = [""] + [input() for _ in range(n)]
+queries = [input() for _ in range(m)]
+
+# Please write your code here.
+d = dict()
+for i, word in enumerate(words):
+    d[word] = i
+
+for query in queries:
+    if query.isdigit():
+        print(words[int(query)])
+    else:
+        print(d[query])
