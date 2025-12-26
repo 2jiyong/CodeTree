60e803aa5edd6e58f40ed9512fda3b6443951e7b
Dino <164735145+2jiyong@users.noreply.github.com>
2025-12-26 20:02:25 +0900

    Title: hashset 기본, Time: 95ms, Memory: 22MB, Status: Wrong Answer - Codetree

--- 251226/hashset 기본/hashset-basic.py
+++ 251226/hashset 기본/hashset-basic.py
@@ -9,9 +9,9 @@ for _ in range(n):
 # Please write your code here.
 s = set()
 for i in range(10):
-    if c == "add":
+    if commands[i] == "add":
         s.add(x[i])
-    if c == "remove":
+    if commands[i] == "remove":
         s.remove(x[i])
-    if c == "find":
-        print(x[i] in s)
+    if commands[i] == "find":
+        print(str(x[i] in s).lower())
