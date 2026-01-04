c109e218faf30e2448afe2466959d5560ec95dd2
Dino <164735145+2jiyong@users.noreply.github.com>
2026-01-04 15:10:39 +0900

    Title: treeset 기본, Time: 89ms, Memory: 19MB, Status: Runtime Error - Codetree

---
+++ 260104/treeset 기본/treeset-basic.py
@@ -0,0 +1,44 @@
+n = int(input())
+command = []
+x = []
+
+for _ in range(n):
+    line = input().split()
+    command.append(line[0])
+    if line[0] in ["add", "remove", "find", "lower_bound", "upper_bound"]:
+        x.append(int(line[1]))
+    else:
+        x.append(0)
+
+
+from sortedcontainers import SortedSet
+# Please write your code here.
+s = SortedSet()
+
+for i in range(n):
+    com = command[i]
+    num = x[i]
+
+    if com == "add":
+        s.add(num)
+    if com == "remove":
+        s.remvoe(num)
+    if com == "find":
+        if num in s:
+            print("true")
+        else:
+            print("false")
+    if com == "lower_bound":
+        print(s[s.bisect_left(num)])
+    if com == "upper_bound":
+        print(s[s.bisect_right(num)])
+    if com == "largest":
+        if s:
+            print(s[-1])
+        else:
+            print("None")
+    if com == "smallest":
+        if s:
+            print(s[0])
+        else:
+            print("None")
